8bd78ea6ed0c51f5ced057f564001a8722403494
Antoine Buteau <antoine.buteau.1@ulaval.ca>
2015-02-23 23:01:18 -0500

    Write Data of different tracks in excel

--- processing.py
+++ processing.py
@@ -12,25 +12,25 @@ def cleanData(df):
 
     df = df[pd.notnull(df['V14'])]   # Removing empty entries from V14- Registration form
     df = df.ix[0:, 'V16':'V189']     #Slicing useful columns and rows
-    df.columns = df.iloc[0]
-    df = df.ix[1:,]
 
     return df
 
 def entriesFormatting(df):
-    #df = df.loc[df['V20'] == 'Social Enterprise (SE)']
-    #valuesToSlice = ['V16', 'V19', 'V20', 'V21', 'V22', 'V102', 'V105', 'V103', 'V109', 'V112', 'V113', 'V114', 'V115', 'V116', 'V117', 'V119']
-    #df = df[valuesToSlice]
-    print(df)
+    valuesToSlice = ['V16', 'V19', 'V20', 'V21', 'V22', 'V102', 'V105', 'V103', 'V109', 'V112', 'V113', 'V114', 'V115', 'V116', 'V117', 'V119', 'V120', 'V123', 'V121', 'V127', 'V130', 'V131', 'V132', 'V133', 'V134', 'V135', 'V137', 'V138', 'V141', 'V139', 'V145', 'V148', 'V149', 'V150', 'V151', 'V152', 'V153', 'V155', 'V156', 'V158', 'V157', 'V162', 'V165', 'V166', 'V167', 'V168', 'V169', 'V170', 'V172', 'V173', 'V175', 'V174', 'V179', 'V182', 'V183', 'V184', 'V185', 'V186', 'V187', 'V189']
+    df = df[valuesToSlice]
 
     return df
 
-def writeToExcel(df, sheetName):
-    writer = ExcelWriter('2015_McGill_Dobson_Cup.xlsx')
-    df.to_excel(writer, sheetName, index=False, startrow=5, startcol=3)
-    writer.save()
+def writeEntriesToExcel(df):
 
+    dfSE = df.loc[df['V20'] == 'Social Enterprise (SE)']
+    dfIDE = df.loc[df['V20'] == 'Innovation Driven Enterprise (IDE)']
+    dfSME = df.loc[df['V20'] == 'Small & Medium Enterprise (SME)']
 
+    with ExcelWriter('2015_McGill_Dobson_Cup.xlsx') as writer:
+        dfSE.to_excel(writer, 'SE', index=False, startrow=5, startcol=3)
+        dfIDE.to_excel(writer, 'IDE', index=False, startrow=5, startcol=3)
+        dfSME.to_excel(writer, 'SME', index=False, startrow=5, startcol=3)
 
 def xlDobson():
     """
@@ -42,7 +42,7 @@ def xlDobson():
 
     df = entriesFormatting(df)
 
-    #writeToExcel(df, "ProcessedData")
+    writeEntriesToExcel(df)
 
 
 if __name__ == "__main__":
