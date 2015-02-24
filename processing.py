629d1334d410ca2bcfeb502142d64c7940dd6bf5
Antoine Buteau <antoine.buteau.1@ulaval.ca>
2015-02-23 21:30:42 -0500

    Slicing relevant data

--- processing.py
+++ processing.py
@@ -4,33 +4,51 @@ import os
 import sys
 from xlwings import Workbook, Range, Sheet
 import pandas as pd
+from pandas import ExcelWriter
 
 
 
+def cleanData(df):
 
-def cleanData():
-    data = Range('2015ColoredData.csv', 'A1').table.value
-    df = pd.DataFrame(data[2:], columns=data[0])
-    numberOfRows = len(df.index)
-    numbersOfColumns = len(df.columns)
+    df = df[pd.notnull(df['V14'])]   # Removing empty entries from V14- Registration form
+    df = df.ix[0:, 'V16':'V189']     #Slicing useful columns and rows
+    df.columns = df.iloc[0]
+    df = df.ix[1:,]
 
-    df = df[pd.notnull(df['V14'])]
-    df = df.ix[:, 'V16':'V189']
+    return df
+
+def entriesFormatting(df):
+    #df = df.loc[df['V20'] == 'Social Enterprise (SE)']
+    #valuesToSlice = ['V16', 'V19', 'V20', 'V21', 'V22', 'V102', 'V105', 'V103', 'V109', 'V112', 'V113', 'V114', 'V115', 'V116', 'V117', 'V119']
+    #df = df[valuesToSlice]
+    print(df)
+
+    return df
+
+def writeToExcel(df, sheetName):
+    writer = ExcelWriter('2015_McGill_Dobson_Cup.xlsx')
+    df.to_excel(writer, sheetName, index=False, startrow=5, startcol=3)
+    writer.save()
+
+
 
 def xlDobson():
     """
     This is a wrapper around fibonacci() to handle all the Excel stuff
     """
-    # Create a reference to the calling Excel Workbook
-    wb = Workbook.caller()
+    df = pd.read_csv('2015_McGill_Dobson_Cup.csv')
 
-    cleanData()
+    df = cleanData(df)
+
+    df = entriesFormatting(df)
+
+    #writeToExcel(df, "ProcessedData")
 
 
 if __name__ == "__main__":
-    if not hasattr(sys, 'frozen'):
-        # The next two lines are here to run the example from Python
-        # Ignore them when called in the frozen/standalone version
-        path = os.path.abspath(os.path.join(os.path.dirname(__file__), '2015_McGill_Dobson_Cup.xlsm'))
-        Workbook.set_mock_caller(path)
+#    if not hasattr(sys, 'frozen'):
+#        # The next two lines are here to run the example from Python
+#        # Ignore them when called in the frozen/standalone version
+#        path = os.path.abspath(os.path.join(os.path.dirname(__file__), '2015_McGill_Dobson_Cup.csv'))
+#        Workbook.set_mock_caller(path)
     xlDobson()
